50c48b913af7d8df5d061f87e34d2a35a76e5a48
George Taylor <george.taylor@digital.justice.gov.uk>
2023-04-26 17:08:27 +0100

    ecr_push

--- terraform/data.tf
+++ terraform/data.tf
@@ -22,9 +22,9 @@ data "aws_subnet" "private_subnets_c" {
   }
 }
 
-data "aws_lb_target_group" "service" {
-  name = var.target_group_name
-}
+# data "aws_lb_target_group" "service" {
+#   name = var.target_group_name
+# }
 
 data "aws_secretsmanager_secret" "connection_string" {
   name = "${local.app_name}-app-connection-string"
