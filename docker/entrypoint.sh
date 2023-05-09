f55e2de6b389ae6734040ddcdf7e6a02bae3d14a
George Taylor <george.taylor@digital.justice.gov.uk>
2023-05-09 20:57:13 +0100

    curl ecr_push

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 echo "starting openldap"
 echo $SLAPD_LOG_LEVEL
-env
+curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
 exec slapd -h "ldap://${LDAP_HOST}:${LDAP_PORT}/ ldapi://${LDAP_HOST}" -u ldap -g ldap -d "${SLAPD_LOG_LEVEL}"
