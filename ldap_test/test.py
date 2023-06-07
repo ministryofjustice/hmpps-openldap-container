1fe1cb1e04b94c17f823b023297704d82c629874
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-07 14:08:07 +0100

    ecr_push test container

---
+++ ldap_test/test.py
@@ -0,0 +1,19 @@
+from ldap3 import Server, Connection, ALL, SUBTREE
+import random
+import os
+
+ldap_server = os.getenv("LDAP_SERVER")
+ldap_user = "cn=root,dc=moj,dc=com"
+ldap_password = os.getenv("BIND_PASSWORD")
+server = Server(ldap_server, get_info=ALL)
+
+for i in range(0, 10000000000):
+    conn = Connection(server, ldap_user, ldap_password, auto_bind=True, authentication="SIMPLE")
+    conn.search(
+        "ou=Users,dc=moj,dc=com",
+        "(&(cn=NDelius" + str(random.randint(0, 100)) + ")(objectClass=person))",
+        search_scope=SUBTREE,
+        attributes=["*"],
+    )
+    print(conn.entries)
+    conn.unbind()
