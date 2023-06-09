fc959f49ee92d715387809108dcfa2d6a2a8c73b
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-09 16:16:13 +0100

    ecr_push test_container

--- ldap_test/test.py
+++ ldap_test/test.py
@@ -6,6 +6,7 @@ ldap_server = os.getenv("LDAP_SERVER")
 ldap_user = "cn=root,dc=moj,dc=com"
 ldap_password = os.getenv("BIND_PASSWORD")
 server = Server(ldap_server, get_info=ALL)
+
 print("Server: " + ldap_server)
 print("User: " + ldap_user)
 print("ldap server info: " + str(server.info))
@@ -13,8 +14,7 @@ print("ldap server info: " + str(server.info))
 
 for i in range(0, 10000000000):
     print("Run #" + str(i))
-    conn=Connection(server, ldap_user, ldap_password,
-                    auto_bind=True, authentication="SIMPLE")
+    conn = Connection(server, ldap_user, ldap_password, auto_bind=True, authentication="SIMPLE")
     conn.search(
         "ou=Users,dc=moj,dc=com",
         "(objectClass=*)",
