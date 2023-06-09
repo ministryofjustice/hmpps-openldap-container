b01f68ff7e58bcc5e6506ccf3b44e990d4a44528
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-09 11:47:50 +0100

    test_container

--- ldap_test/test.py
+++ ldap_test/test.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 from ldap3 import Server, Connection, ALL, SUBTREE
 import random
 import os
@@ -12,8 +9,7 @@ server = Server(ldap_server, get_info=ALL)
 
 for i in range(0, 10000000000):
     print("Run #" + str(i))
-    conn = Connection(server, ldap_user, ldap_password,
-                      auto_bind=True, authentication="SIMPLE")
+    conn = Connection(server, ldap_user, ldap_password, auto_bind=True, authentication="SIMPLE")
     conn.search(
         "ou=Users,dc=moj,dc=com",
         "(&(cn=NDelius" + str(random.randint(0, 100)) + ")(objectClass=person))",
