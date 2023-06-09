c0ee5b77b456631a55eb10a190f7c197f6339620
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-09 15:59:31 +0100

    test_container

--- ldap_test/test.py
+++ ldap_test/test.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 from ldap3 import Server, Connection, ALL, SUBTREE
 import random
 import os
@@ -9,14 +6,17 @@ ldap_server = os.getenv("LDAP_SERVER")
 ldap_user = "cn=root,dc=moj,dc=com"
 ldap_password = os.getenv("BIND_PASSWORD")
 server = Server(ldap_server, get_info=ALL)
+print("Server: " + ldap_server)
+print("User: " + ldap_user)
+print("ldap server info: " + str(server.info)
+
 
 for i in range(0, 10000000000):
     print("Run #" + str(i))
-    conn = Connection(server, ldap_user, ldap_password,
-                      auto_bind=True, authentication="SIMPLE")
+    conn = Connection(server, ldap_user, ldap_password, auto_bind=True, authentication="SIMPLE")
     conn.search(
         "ou=Users,dc=moj,dc=com",
-        "(objectClass=person)",
+        "(objectClass=*)",
         search_scope=SUBTREE,
         attributes=["*"],
     )
