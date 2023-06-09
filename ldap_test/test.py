169320b533ade9d1ce20aa06f44d89792fc54c4d
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-09 16:57:32 +0100

    extra ) test_container ecr_push

--- ldap_test/test.py
+++ ldap_test/test.py
@@ -21,7 +21,7 @@ for i in range(0, 10000000000):
     conn = Connection(server, ldap_user, ldap_password, auto_bind=True, authentication="SIMPLE")
     conn.search(
         "ou=Users,dc=moj,dc=com",
-        "(&(cn=" + random.choice(user_list) + "*))(objectClass=person))",
+        "(&(cn=" + random.choice(user_list) + "*)(objectClass=person))",
         search_scope=SUBTREE,
         attributes=["*"],
     )
