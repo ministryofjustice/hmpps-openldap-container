3023a27f1628f79034699e9393a0c20a1d66f2ef
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-13 11:39:41 +0100

    tidy test

--- ldap_test/test.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-ldapsearch -L -x -D "cn=root,dc=moj,dc=com" -w ${BIND_PASSWORD} -H "ldap://delius-core-openldap-nlb-031548f823b8589d.elb.eu-west-2.amazonaws.com:389" -b "ou=Users,dc=moj,dc=com" -s sub "(&(cn=George*)(objectClass=person))"
