6bb44c8103bfcdc32ab8634b34734be8dbb16362
Seb Norris <sebastian@automationlogic.com>
2024-08-02 15:20:53 +0100

    update ssm command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ echo $SLAPD_LOG_LEVEL
 IP=$LDAP_HOST
 
 echo "setting RBAC tag..."
-RBAC_TAG=$(aws ssm get-parameters --name "/$DELIUS_ENVIRONMENT/LDAP_RBAC_VERSION" --region eu-west-2 --query "Parameter.Value" --output text)
+RBAC_TAG=$(aws ssm get-parameter --name "/$DELIUS_ENVIRONMENT/LDAP_RBAC_VERSION" --region eu-west-2 --query "Parameter.Value" --output text --with-decryption)
 echo "RBAC tag is $RBAC_TAG"
 
 echo "Cloning rbac repo..."
