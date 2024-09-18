d2088d088ed35edcf3041b6ae143fe809f8df02f
Seb Norris <sebastian@automationlogic.com>
2024-09-18 17:19:08 +0100

    remove resources as deployment moved to mp repo

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "ecs_cluster_arn" {
-  value = "arn:aws:ecs:eu-west-2:${data.aws_caller_identity.current.id}:cluster/${var.namespace}-${var.environment}-cluster"
-}
+# output "ecs_cluster_arn" {
+#   value = "arn:aws:ecs:eu-west-2:${data.aws_caller_identity.current.id}:cluster/${var.namespace}-${var.environment}-cluster"
+# }
 
-output "ecs_service_arn" {
-  value = module.deploy.service_arn
-}
+# output "ecs_service_arn" {
+#   value = module.deploy.service_arn
+# }
