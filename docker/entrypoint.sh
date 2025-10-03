92025b6208e4ad57f904a49b83c414fcb3c6fee2
David Teeling <david.teeling1@justice.gov.uk>
2025-10-03 11:00:27 +0100

    Test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -21,7 +21,6 @@ warmup_ldap_cache() {
 
 start_sladp() {
     slapd -F /etc/openldap/slapd.d -h "ldap://${IP}:${LDAP_PORT}/ ldapi://%2Fvar%2Flib%2Fopenldap%2Frun%2Fldapi" -d $SLAPD_LOG_LEVEL &
-    SLAPD_PID=$(cat /var/run/openldap/slapd.pid)
 
     echo "Waiting for OpenLDAP to start"
     while true; do
@@ -32,6 +31,7 @@ start_sladp() {
 
     warmup_ldap_cache
 
+    SLAPD_PID=$(cat /var/run/openldap/slapd.pid)
     wait $SLAPD_PID
 }
 
